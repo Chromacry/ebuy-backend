dd0d35da2be54fc3057abc056bbace777ead48b4
Scott <sithuaung2002@gmail.com>
2023-11-15 13:13:00 +0800

    added DBConfig.js in the ./src/routes

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,7 @@
 // index.mjs
 import express from 'express';
 import dotenv from 'dotenv';
-
+import DBConfig from './routes/DBConfig.mjs';
 
 dotenv.config({ path: `.env.local`, override: true });
 
@@ -18,3 +18,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
