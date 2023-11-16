55244b170f53bbbc043e497d1557428a4eb338d8
Alston <alstonsong0@gmail.com>
2023-11-16 16:29:32 +0800

    feat: added get all reviews of a product id and route for it

--- src/controllers/ReviewController.mjs
+++ src/controllers/ReviewController.mjs
@@ -5,8 +5,10 @@ import { Review } from "../models/ReviewModel.mjs";
 const reviewDao = new ReviewDao();
 export const addReview = (req, res) => {
   try {
-    var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-    var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+    var tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
+    var localISOTime = new Date(Date.now() - tzoffset)
+      .toISOString()
+      .slice(0, -1);
     console.log(localISOTime);
     const model = new Review(
       null,
@@ -22,6 +24,28 @@ export const addReview = (req, res) => {
       if (error) throw new Error(error);
       res.json({
         message: "Successfully added Review!",
+        data: result,
+        status: STATUS_CODES.SUCCESS_CODE,
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      message: `An unexpected error occurred: ${error}`,
+      status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
+    });
+  }
+};
+
+export const getProductReviews = (req, res) => {
+  try {
+    const model = new Review();
+    model.setProductId(req.params.id);
+    console.log(model.getProductId());
+    reviewDao.getProductReviews(model, (error, result) => {
+      if (error) throw new Error(error);
+      res.json({
+        message: "Successfully retrieved reviews!",
         data: result,
         status: STATUS_CODES.SUCCESS_CODE,
       });
