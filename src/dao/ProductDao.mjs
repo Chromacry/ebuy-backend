6a34104fcbe3a68713d9b7af719d1a662901ac4f
Chromacry <Chromacry101@gmail.com>
2023-11-25 13:30:03 +0800

    feat: added update route; added update controller and update dao; added checkif id exist;

--- src/dao/ProductDao.mjs
+++ src/dao/ProductDao.mjs
@@ -30,4 +30,22 @@ export class ProductDao {
     dbConnection.query(sql, [model.getSellerId(), model.getProductName()], callback);
     dbConnection.end();
   }
+
+  getProductById(model, callback) 
+  {
+    const dbConnection = mysql.createConnection(dbConfig);
+    dbConnection.connect();
+    const sql = `SELECT * FROM ${dbTableName} WHERE seller_id = ? AND product_name = ?`;
+    dbConnection.query(sql, [model.getSellerId(), model.getProductName()], callback);
+    dbConnection.end();
+  }
+
+  updateProduct(model, callback) 
+  {
+    const dbConnection = mysql.createConnection(dbConfig);
+    dbConnection.connect();
+    const sql = `UPDATE ${dbTableName} SET product_name = COALESCE(?, product_name), product_description = COALESCE(?, product_description), product_image = COALESCE(?, product_image) WHERE id = ? AND seller_id = ? `;
+    dbConnection.query(sql, [model.getProductName(), model.getProductDescription(), model.getProductImage(), model.getId(), model.getSellerId()], callback);
+    dbConnection.end();
+  }
 }
--- src/routes/ProductRoute.mjs
+++ src/routes/ProductRoute.mjs
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { register } from "../controllers/UserController.mjs";
-import { addProduct, getAllProducts } from "../controllers/ProductController.mjs";
+import { addProduct, getAllProducts, updateProduct } from "../controllers/ProductController.mjs";
 const router = Router();
 
 router.get("/get/all", getAllProducts);
 router.post("/add", addProduct);
+router.post("/update", updateProduct);
 
 export default router;
