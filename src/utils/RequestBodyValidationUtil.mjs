a7f3b17f8a69b87fe355e43e7507494cf071466c
Chromacry <Chromacry101@gmail.com>
2023-11-26 01:15:20 +0800

    fix: changed res.json to return for add product validation;

--- src/utils/RequestBodyValidationUtil.mjs
+++ src/utils/RequestBodyValidationUtil.mjs
@@ -4,39 +4,34 @@ export class ProductValidations {
   addProductValidator(body, res) {
     //* Validation Check
     if (!body?.seller_id && Number.isInteger(body?.seller_id)) {
-      res.json({
+      return {
         message: "sellerId field required!, field-type: Integer",
         status: STATUS_CODES.BAD_REQUEST_CODE,
-      });
-      return;
+      }
     }
     if (!body?.product_name) {
-      res.json({
+      return {
         message: "productName field required!",
         status: STATUS_CODES.BAD_REQUEST_CODE,
-      });
-      return;
+      }
     }
     if (!body?.product_description) {
-      res.json({
+      return {
         message: "productDescription field required!",
         status: STATUS_CODES.BAD_REQUEST_CODE,
-      });
-      return;
+      }
     }
     if (!body?.product_image) {
-      res.json({
+      return {
         message: "productImage field required!",
         status: STATUS_CODES.BAD_REQUEST_CODE,
-      });
-      return;
+      }
     }
     if (!body?.product_quantity && Number.isInteger(body?.product_quantity)) {
-      res.json({
+      return {
         message: "productQuantity field required!, field-type: Integer",
         status: STATUS_CODES.BAD_REQUEST_CODE,
-      });
-      return;
+      }
     }
   }
 
