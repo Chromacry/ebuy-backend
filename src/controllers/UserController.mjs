c67f6f247eedd234090f556e0c31c7cf18db5b73
Scott <sithuaung2002@gmail.com>
2023-11-26 15:34:43 +0800

    refactor: refactored all the features in the userController to use the DAO

--- src/controllers/UserController.mjs
+++ src/controllers/UserController.mjs
@@ -6,190 +6,129 @@ import crypto from "crypto";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { STATUS_CODES } from "../constants/GlobalConstants.mjs";
-
+import { UserDao } from "../dao/UserDao.mjs";
+import { User } from "../models/UserModel.mjs";
 import { getDateTimeNowLocalISOString } from "../utils/DateTimeUtil.mjs";
 dotenv.config({ path: `.env.local`, override: true });
 
 export const register = async (req, res) => {
-  const dbConnection = mysql.createConnection(dbConfig);
+  const userDao = new UserDao();
   const { username, email, password } = req.body;
 
-  try {
-    // Validation checks
-    if (!username || !email || !password) {
-      return res.json({
-        message: "Username, email, and password are required!",
-        status: STATUS_CODES.BAD_REQUEST_CODE,
-      });
-    }
-
-    // Email format validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      return res.json({
-        message: "Invalid email format!",
-        status: STATUS_CODES.BAD_REQUEST_CODE,
-      });
-    }
-
-    dbConnection.connect();
-
-    const emailCheck = await new Promise((resolve, reject) => {
-      dbConnection.query(
-        "SELECT email FROM users WHERE email = ?",
-        [email],
-        (err, result) => {
-          if (err) {
-            reject({
-              message: "Database error!",
-              status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-            });
-          } else {
-            resolve(result[0]);
-          }
-        }
-      );
+  if (!username || !email || !password) {
+    return res.json({
+      message: "Username, email, and password are required!",
+      status: STATUS_CODES.BAD_REQUEST_CODE,
     });
-
-    if (emailCheck) {
+  }
+  // Email format validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.json({
+      message: "Invalid email format!",
+      status: STATUS_CODES.BAD_REQUEST_CODE,
+    });
+  }
+  try {
+    const model = new User(
+      null,
+      username,
+      email,
+      null,
+      null,
+      false,
+      null,
+      getDateTimeNowLocalISOString()
+    );
+    const emailExists = await userDao.checkEmailExists(model);
+    if (emailExists) {
       return res.json({
         message: "Email has already been registered!",
         status: STATUS_CODES.BAD_REQUEST_CODE,
       });
-    } else {
-      const EncryptedPassword = await bcrypt.hash(password, 10);
-
-      await new Promise((resolve, reject) => {
-        dbConnection.query(
-          "INSERT INTO users SET ?",
-          {
-            username: username,
-            email: email,
-            password: EncryptedPassword,
-            created_time: getDateTimeNowLocalISOString(), // Add the created_time field with the formatted date
-          },
-          (error, results) => {
-            if (error) {
-              reject({
-                message: "Database error!",
-                status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-              });
-            } else {
-              resolve(results);
-            }
-          }
-        );
-      });
-
-      return res.json({
-        message: "Registration successful!",
-        status: STATUS_CODES.SUCCESS_CODE,
-      });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    model.setPassword(hashedPassword);
+    await userDao.addUser(model);
+
+    return res.json({
+      message: "Registration successful!",
+      status: STATUS_CODES.SUCCESS_CODE,
+    });
   } catch (error) {
     console.error("Error during registration:", error);
     return res.json({
       message: "Internal server error!",
       status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
     });
-  } finally {
-    dbConnection.end();
   }
 };
 
 export const login = async (req, res) => {
-  const dbConnection = mysql.createConnection(dbConfig);
+  const userDao = new UserDao();
   const { email, password } = req.body;
-  dbConnection.connect();
+
+  // Validation checks
+  if (!email || !password) {
+    return res.json({
+      message: "Email and password are required!",
+      status: STATUS_CODES.BAD_REQUEST_CODE,
+    });
+  }
+
+  // Email format validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.json({
+      message: "Invalid email format!",
+      status: STATUS_CODES.BAD_REQUEST_CODE,
+    });
+  }
 
   try {
-    // Validation checks
-    if (!email || !password) {
-      return res.json({
-        message: "Email and password are required!",
-        status: STATUS_CODES.BAD_REQUEST_CODE,
-      });
-    }
-
-    // Email format validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      return res.json({
-        message: "Invalid email format!",
-        status: STATUS_CODES.BAD_REQUEST_CODE,
-      });
-    }
-
-    const userResult = await new Promise((resolve, reject) => {
-      dbConnection.query(
-        "SELECT * FROM users WHERE email = ?",
-        [email],
-        (err, result) => {
-          if (err) {
-            reject({
-              message: "Database error!",
-              status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-            });
-          } else {
-            resolve(result);
-          }
-        }
-      );
-    });
-
-    if (
-      !userResult.length ||
-      !(await bcrypt.compare(password, userResult[0].password))
-    ) {
+    const model = new User(null, null, email);
+    const user = await userDao.getUserByEmail(model);
+    if (user == 0) {
       return res.json({
         message: "Incorrect Email or Password!",
         status: STATUS_CODES.BAD_REQUEST_CODE,
       });
-    } else {
-      let userToken = crypto.randomBytes(64).toString("hex");
-      await new Promise((resolve, reject) => {
-        dbConnection.query(
-          "UPDATE users SET token = ? WHERE email = ?",
-          [userToken, email],
-          (err, result) => {
-            if (err) {
-              reject({
-                message: "Database error!",
-                status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-              });
-            } else {
-              resolve(result);
-            }
-          }
-        );
-      });
-
-      const token = jwt.sign(
-        { id: userResult[0].id, token: userToken },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: process.env.JWT_EXPIRES,
-        }
-      );
-
+    }
+    if (!(await bcrypt.compare(password, user[0].password))) {
       return res.json({
-        message: "Login successful!",
-        status: STATUS_CODES.SUCCESS_CODE,
-        token: token,
+        message: "Incorrect Email or Password!",
+        status: STATUS_CODES.BAD_REQUEST_CODE,
       });
     }
+
+    const userToken = crypto.randomBytes(64).toString("hex");
+    model.setToken(userToken);
+    model.setId(user[0].id);
+    await userDao.updateUserToken(model);
+
+    const token = jwt.sign(
+      { id: user[0].id, token: userToken },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES }
+    );
+
+    return res.json({
+      message: "Login successful!",
+      status: STATUS_CODES.SUCCESS_CODE,
+      token: token,
+    });
   } catch (error) {
     console.error("Error during login:", error);
     return res.status(error.status || 500).json({
       status: error.status || 500,
       message: error.message || "Internal server error!",
     });
-  } finally {
-    dbConnection.end();
   }
 };
 
 export const getUser = async (req, res) => {
+  const userDao = new UserDao();
   const storedToken = req.headers.token;
 
   if (!storedToken) {
@@ -199,35 +138,24 @@ export const getUser = async (req, res) => {
     });
   }
 
-  const dbConnection = mysql.createConnection(dbConfig);
-
   try {
     const decoded = jwt.verify(storedToken, process.env.JWT_SECRET);
-    dbConnection.connect();
+    const model = new User(decoded.id, null, null, null, decoded.token);
+    const user = await userDao.getUserByIdAndToken(model);
 
-    const result = await new Promise((resolve, reject) => {
-      dbConnection.query(
-        "SELECT username, email, profile_image, id, created_time FROM users WHERE id = ? AND token = ?",
-        [decoded.id, decoded.token],
-        (err, result) => {
-          if (err) {
-            reject({
-              message: "Database error!",
-              status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-            });
-          } else {
-            resolve(result);
-          }
-        }
-      );
-    });
+    if (!user) {
+      return res.status(401).json({
+        message: "Unauthorized: Invalid token or user not found!",
+        status: STATUS_CODES.UNAUTHORIZED_CODE,
+      });
+    }
 
     return res.json({
-      username: result[0].username,
-      profile_image: result[0].profile_image,
-      email: result[0].email,
-      id: result[0].id,
-      created_time: result[0].created_time,
+      username: user[0].username,
+      profile_image: user[0].profile_image,
+      email: user[0].email,
+      id: user[0].id,
+      created_time: user[0].created_time,
     });
   } catch (error) {
     if (error instanceof jwt.JsonWebTokenError) {
@@ -241,21 +169,20 @@ export const getUser = async (req, res) => {
       status: error.status || 500,
       message: error.message || "Internal server error!",
     });
-  } finally {
-    dbConnection.end();
   }
 };
 
 export const passwordReset = async (req, res) => {
   const storedToken = req.headers.token;
+  const userDao = new UserDao();
 
   try {
-    const decoded = await jwt.verify(storedToken, process.env.JWT_SECRET);
+    const decoded = jwt.verify(storedToken, process.env.JWT_SECRET);
     const currentPassword = req.body.currentPassword;
     const newPassword = req.body.newPassword;
     const confirmPassword = req.body.confirmPassword;
 
-    // Validation checks
+    // Validation checks...
     if (!currentPassword || !newPassword || !confirmPassword) {
       return res.json({
         message: "All fields are required!",
@@ -277,30 +204,9 @@ export const passwordReset = async (req, res) => {
       });
     }
 
-    const dbConnection = mysql.createConnection(dbConfig);
-    dbConnection.connect();
-
-    const result = await new Promise((resolve, reject) => {
-      dbConnection.query(
-        "SELECT * FROM users WHERE id = ? AND token = ?",
-        [decoded.id, decoded.token],
-        (err, result) => {
-          if (err) {
-            reject({
-              message: "Database error!",
-              status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-            });
-          } else {
-            resolve(result);
-          }
-        }
-      );
-    });
-
-    if (
-      !result.length ||
-      !(await bcrypt.compare(currentPassword, result[0].password))
-    ) {
+    const model = new User(decoded.id, null, null, null, decoded.token);
+    const user = await userDao.getUserByIdAndToken(model);
+    if (!user || !(await bcrypt.compare(currentPassword, user[0].password))) {
       return res.json({
         message: "Incorrect Entries!",
         status: STATUS_CODES.BAD_REQUEST_CODE,
@@ -308,23 +214,9 @@ export const passwordReset = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(newPassword, 10);
-    await new Promise((resolve, reject) => {
-      dbConnection.query(
-        "UPDATE users SET password = ? WHERE id = ?",
-        [hashedPassword, decoded.id],
-        (err, result) => {
-          if (err) {
-            reject({
-              message: "Database error!",
-              status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-            });
-          } else {
-            resolve(result);
-          }
-        }
-      );
-    });
-    dbConnection.end();
+    model.setEmail(user[0].id);
+    model.setPassword(hashedPassword);
+    await userDao.updateUserPassword(model);
 
     return res.json({
       message: "Password Reset Successfully!",
@@ -339,7 +231,6 @@ export const passwordReset = async (req, res) => {
       });
     }
 
-    // Generic error handling
     return res.status(error.status || 500).json({
       status: error.status || 500,
       message: error.message || "Internal server error!",
@@ -348,11 +239,13 @@ export const passwordReset = async (req, res) => {
 };
 
 export const updateUsername = async (req, res) => {
-  const storedToken = req.headers.token;
+  const userDao = new UserDao();
 
   try {
-    const decoded = await jwt.verify(storedToken, process.env.JWT_SECRET);
+    const storedToken = req.headers.token;
     const newUsername = req.body.username;
+    const decoded = jwt.verify(storedToken, process.env.JWT_SECRET);
+    const model = new User(decoded.id, null, null, null, decoded.token);
 
     // Validation check for empty username
     if (!newUsername) {
@@ -362,72 +255,30 @@ export const updateUsername = async (req, res) => {
       });
     }
 
-    const dbConnection = mysql.createConnection(dbConfig);
-    dbConnection.connect();
+    const currentUser = await userDao.getUserByIdAndToken(model);
 
-    // Querying the database to find the current username
-    const currentUser = await new Promise((resolve, reject) => {
-      dbConnection.query(
-        "SELECT username FROM users WHERE id = ? AND token = ?",
-        [decoded.id, decoded.token],
-        (err, result) => {
-          if (err) {
-            reject({
-              message: "Database query error!",
-              status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-            });
-          } else {
-            resolve(result);
-          }
-        }
-      );
-    });
-
-    // Check if the new username is the same as the current username
     if (currentUser.length > 0 && currentUser[0].username === newUsername) {
-      dbConnection.end();
       return res.json({
         message: "New username cannot be the same as the current username!",
         status: STATUS_CODES.BAD_REQUEST_CODE,
       });
     }
 
-    dbConnection.connect();
-    // Updating the username in the database
-    const updateResult = await new Promise((resolve, reject) => {
-      dbConnection.query(
-        "UPDATE users SET username = ? WHERE id = ?",
-        [newUsername, decoded.id],
-        (err, result) => {
-          if (err) {
-            reject({
-              message: "Username update error!",
-              status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-            });
-          } else {
-            resolve(result);
-          }
-        }
-      );
-    });
-    dbConnection.end();
+    model.setId(decoded.id);
+    model.setUsername(newUsername);
+    await userDao.updateUsername(model);
 
-    if (updateResult) {
-      return res.json({
-        message: "Username updated successfully!",
-        status: STATUS_CODES.SUCCESS_CODE,
-      });
-    }
+    return res.json({
+      message: "Username updated successfully!",
+      status: STATUS_CODES.SUCCESS_CODE,
+    });
   } catch (error) {
-    // Handling JWT Token Decoding Error
     if (error instanceof jwt.JsonWebTokenError) {
       return res.status(401).json({
         message: "Unauthorized: Invalid or Expired Token!",
         status: STATUS_CODES.UNAUTHORIZED_CODE,
       });
     }
-
-    // Generic error handling
     return res.status(error.status || 500).json({
       status: error.status || 500,
       message: error.message || "Internal server error!",
@@ -436,45 +287,37 @@ export const updateUsername = async (req, res) => {
 };
 
 export const updateProfileImage = async (req, res) => {
+  const userDao = new UserDao();
   const storedToken = req.headers.token;
 
   try {
-    const decoded = await jwt.verify(storedToken, process.env.JWT_SECRET);
-    const profile_image = req.body.profileImg;
+    const decoded = jwt.verify(storedToken, process.env.JWT_SECRET);
+    const profileImage = req.body.profileImg;
 
     // Validation check
-    if (!profile_image) {
+    if (!profileImage) {
       return res.json({
         message: "Profile image cannot be empty!",
         status: STATUS_CODES.BAD_REQUEST_CODE,
       });
     }
 
-    const dbConnection = mysql.createConnection(dbConfig);
-    dbConnection.connect();
+    const model = new User(
+      decoded.id,
+      null,
+      null,
+      null,
+      decoded.token,
+      null,
+      profileImage,
+      null
+    );
+    await userDao.updateProfileImage(model);
 
-    const result = await new Promise((resolve, reject) => {
-      dbConnection.query(
-        "UPDATE users SET ? WHERE id = ?",
-        [{ profile_image }, decoded.id],
-        (err, result) => {
-          if (err) {
-            reject({ message: "Profile image update error!", status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE });
-          } else {
-            resolve(result);
-          }
-        }
-      );
+    return res.json({
+      message: "Profile image updated successfully!",
+      status: STATUS_CODES.SUCCESS_CODE,
     });
-
-    dbConnection.end();
-
-    if (result) {
-      return res.json({
-        message: "Profile image updated successfully!",
-        status: STATUS_CODES.SUCCESS_CODE,
-      });
-    }
   } catch (error) {
     // Handling JWT Token Decoding Error
     if (error instanceof jwt.JsonWebTokenError) {
@@ -484,7 +327,6 @@ export const updateProfileImage = async (req, res) => {
       });
     }
 
-    // Generic error handling
     return res.status(error.status || 500).json({
       status: error.status || 500,
       message: error.message || "Internal server error!",
@@ -492,32 +334,16 @@ export const updateProfileImage = async (req, res) => {
   }
 };
 
-
 export const deleteAccount = async (req, res) => {
+  const userDao = new UserDao();
   const storedToken = req.headers.token;
 
   try {
-    const decoded = await jwt.verify(storedToken, process.env.JWT_SECRET);
-    const dbConnection = mysql.createConnection(dbConfig);
-    dbConnection.connect();
+    const decoded = jwt.verify(storedToken, process.env.JWT_SECRET);
+    const model = new User(decoded.id, null, null, null, decoded.token);
 
-    const result = await new Promise((resolve, reject) => {
-      dbConnection.query(
-        "DELETE FROM users WHERE id = ? AND token = ?",
-        [decoded.id, decoded.token],
-        (err, result) => {
-          if (err) {
-            reject({
-              message: "Database error!",
-              status: STATUS_CODES.INTERNAL_SERVER_ERROR_CODE,
-            });
-          } else {
-            resolve(result);
-          }
-        }
-      );
-    });
-    dbConnection.end();
+    const result = await userDao.deleteUser(model);
+
     if (result.affectedRows > 0) {
       return res.json({
         message: "Account Deleted Successfully!",
