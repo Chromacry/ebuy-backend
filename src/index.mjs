b5e9186c3d527ea960ff2a4471a896f84aa0e20b
Scott <sithuaung2002@gmail.com>
2023-12-03 15:03:38 +0800

    feat: added the app to use cors

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,6 @@
 // index.mjs
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config({ path: `.env.local`, override: true });
 
@@ -11,6 +12,7 @@ const port = process.env.SERVER_PORT || 8080;
 app.use(express.json({
   limit: '100mb'
 }))
+app.use(cors())
 app.use(express.urlencoded({ extended: true}))
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
