a28c34191f44b96ec70dd9fb0bb8efdf0be9e658
Chromacry <Chromacry101@gmail.com>
2024-01-24 18:23:19 +0800

    feat: added pagination to product; removed seller_id from validation

--- src/routes/ProductRoute.mjs
+++ src/routes/ProductRoute.mjs
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { register } from "../controllers/UserController.mjs";
-import { addProduct, deleteProduct, getAllProducts, getProduct, updateProduct } from "../controllers/ProductController.mjs";
+import { addProduct, deleteProduct, getAllProductList, getAllProducts, getProduct, updateProduct } from "../controllers/ProductController.mjs";
 const router = Router();
 
 router.get("/get/all", getAllProducts);
+router.get("/get/list", getAllProductList);
 router.get("/get", getProduct);
 router.post("/add", addProduct);
 router.delete("/delete", deleteProduct);
