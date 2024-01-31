3a882a552a673f28650075a8c39eaca2025570e4
Alston <alstonsong0@gmail.com>
2024-02-01 01:14:39 +0800

    refactor: terraform

--- projectx-backend-terraform/main.tf
+++ projectx-backend-terraform/main.tf
@@ -12,18 +12,3 @@ resource "azurerm_resource_group" "dvopsResourceGroup" {
 	name = "dvopsResourceGroup"
 	location = "East US"
 }
-resource "azurerm_kubernetes_cluster" "dvopsAKSCluster" {
-	name = "dvopsAKSCluster"
-	location = azurerm_resource_group.dvopsResourceGroup.location
-	resource_group_name = azurerm_resource_group.dvopsResourceGroup.name
-	dns_prefix = "rms-aks"
-	default_node_pool {
-		name = "default"
-		node_count = 1
-		vm_size = "Standard_DS2_v2"
-	}
-	service_principal {
-		client_id = var.azure_app_id
-		client_secret = var.azure_password
-	}
-}
